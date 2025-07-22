dff63316842087564b479b21fceb6ec492095ddd
Dario Piotrowicz <dario@cloudflare.com>
2025-07-23 00:41:43 +0100

    avoid importing open-next in config for Vercel deployments

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,19 @@
+let deploymentId = undefined;
+
+if (!process.env.VERCEL) {
+    const { getDeploymentId } = await import('@opennextjs/cloudflare')
+    deploymentId = getDeploymentId();
+}
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    deploymentId: process.env.VERCEL ? undefined : getDeploymentId(),
+    deploymentId,
 };
 
 export default nextConfig;
 
-// added by create cloudflare to enable calling `getCloudflareContext()` in `next dev`
-import { getDeploymentId, initOpenNextCloudflareForDev } from '@opennextjs/cloudflare';
-initOpenNextCloudflareForDev();
+if (!process.env.VERCEL) {
+    // added by create cloudflare to enable calling `getCloudflareContext()` in `next dev`
+    const { initOpenNextCloudflareForDev } = await import('@opennextjs/cloudflare');
+    await initOpenNextCloudflareForDev();
+}
